3b30c02cb9ced99662596f732182aefdedb99c8a
hermenegildoAng <hernandezhermenegildoangeldavi@gmail.com>
2026-03-25 10:51:41 -0600

    correo corecion

--- core/settings.py
+++ core/settings.py
@@ -153,16 +153,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # ─── EMAIL ────────────────────────────────────────────────────
-EMAIL_BACKEND   = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
-EMAIL_HOST      = config('EMAIL_HOST', default='smtp-relay.brevo.com')
-EMAIL_PORT      = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_BACKEND   = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST      = 'smtp.gmail.com'
+EMAIL_PORT      = 587
+EMAIL_USE_TLS = True
 EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL  = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+EMAIL_HOST_USER = 'mcarmonapalestina@gmail.com'
+EMAIL_HOST_PASSWORD = 'tlttdawavhhwzydh'
+DEFAULT_FROM_EMAIL  = EMAIL_HOST_USER
 FRONTEND_URL    = config('FRONTEND_URL', default='http://localhost:5173')
-PASSWORD_RESET_TIMEOUT = 86400
+PASSWORD_RESET_TIMEOUT = 259200
 
 # ─── INTERNACIONALIZACIÓN ─────────────────────────────────────
 LANGUAGE_CODE = 'es-mx'
